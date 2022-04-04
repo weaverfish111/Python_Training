cafb10a8a7bd5d26267e237551ed464b9742c88a
weaverfish111 <78041472+weaverfish111@users.noreply.github.com>
2022-04-04 19:52:56 +0200

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -3,6 +3,8 @@
 ## 18/03/2022
 
 import random
+import matplotlib.pyplot as plt	
+
 from turtle import position
 
 def throw_two_dice():
@@ -15,83 +17,75 @@ def throw_two_dice():
     # print(f"throw = {roll}")
     # print(f"The Total Value of the Dice Roll Was {roll}")
     if dice_1 == dice_2:
-        print("A Double Has Been Thrown!")
+        # print("A Double Has Been Thrown!")
         double = 1
     return(roll)
 
 def simulate_monopoly():
     position = 0
     throw = 0
-    completed_list = list(range(41))
-    # throw = throw_two_dice()
+    properties_left = 28
+    board_values = [0, 60, 0, 60, 0, 200, 100, 0, 100, 120, 0, 140, 150, 140, 160, 200, 180,
+                    0, 180, 200, 0, 220, 0, 220, 240, 200, 260, 260, 150, 280, 0, 300, 300,
+                    0, 320, 200, 0, 350, 0, 400]
+    possessions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+    completed_list = list(range(40))    
     position_list = []
-    while position <= 40:
+    position_value = 0
+    # print(completed_list)
+    ## While looped actioning simulation
+    # while possessions != board_values:
+    while properties_left != 0: ##change to properties left
         throw += 1
         position += throw_two_dice()
-        print(f"On Throw {throw} the Position is {position}")
-        if position not in position_list:
-            position_list.append(position)
-            print(position_list)
-        elif position > 40:
-            position = position % 40
-        if position_list == completed_list:
+        # print(f"on Throw {throw} Position is {position}")
+        ## If function here (if over 40 minus 40)
+        if position >= 40:
+            position = position - 40 
+
+        # if position not in position_list:
+            # Must sort list position.list!
+            ## Will change to only add if money is allowing.
+            # position_list.append(position)
+            # position_list.sort()
+            # print(position_list)
+
+            # Trying to create corrolation between position and board_value
+            # position_value = board_values[position]         ### Something may be wrong here... why it is not inputting correct
+                                                            ### The position is changing, because the list is an empty list being added to...
+                                                            ### Position list index 1 is not constant...
+                                                            ### But it works the first go round, but not after...? Why?
+
+        
+        if board_values[position] > 0 and possessions[position] == 0:
+            possessions[position] = board_values[position]
+            # print("Property Bought!")
+            properties_left -= 1
+            # print(possessions)
+            # print(f"Number of properties left = {properties_left}")
+
+        if possessions == board_values:
             print("done")
-    return(position_list)
+        
+        # print(f"On Throw {throw} the Position is {position} (Value = {position_value})")
+        # print(len(position_list))
+        # print(position_list)
+        # print(completed_list)
+    return(throw) ##Show how many throws it take to buy all properties (not inclusing spots like jail(20) and start (0)
 
-    #     elif position + position >= 40:
-    #         position = position - 40
-    #     elif position_list == completed_list:
-    #         print("done")
-            
-    #     # print(position)
-    # # print(position_list)
-    # return(position_list)
+def simulate_monopoly_games(total_games):
+    number_of_throws_for_completion = []
+    for game in range(0, total_games):
+        number_of_throws = simulate_monopoly()
+        number_of_throws_for_completion.append(number_of_throws)
 
-print(simulate_monopoly())
-# print(throw_two_dice())
-
-
-
-
-
-
-
-
-
-
-# def practice_with_dice():
-#     double_count = 0
-#     for throws in range(0, 1000, 1):
-#         throws = throw_two_dice()
-#         # print(throws)
-#         double_count += throw_two_dice()
-#     print(double_count)
-#     return(double_count)
+    ## Histogram Graph showing Number of Turns
+    plt.hist(number_of_throws_for_completion)
+    plt.title(f'Histogram showing Number of Turns taken in {total_games} Games of Monopoly')
+    plt.xlabel('Number Of Turns Taken')
+    plt.ylabel('Number of Number of Games')
+    plt.show()
+    return(number_of_throws_for_completion)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Need to work out how to call variable from 'throw_two_dice' function into this function!
-#         if double_count >= 0:
-#             print(f"Number of doubles is {double_count}")
-    
-
-
-# print(practice_with_dice())
-
-
-# def simulate_monopoly_games(total_games):
-#     # total_games = #number of games being simulated
-#     return(#number of throws required before all streets were held by the player.)
+print(simulate_monopoly_games(10000))
